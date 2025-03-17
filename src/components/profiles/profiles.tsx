394faa1ec7ddb61fc69eb30eaeb733670c1be87b
Mu12Lambda11 <idjordan@cpp.edu>
2025-03-16 22:08:03 -0700

    Implemented full profile section

--- src/components/profiles/profiles.tsx
+++ src/components/profiles/profiles.tsx
@@ -1,5 +1,8 @@
 import styles from './profiles.module.scss';
 import cx from 'classnames';
+import JacoboHerreraJpg from '../../assets/img/profiles/jacobo herrera.jpg';
+import SamPerezPng from '../../assets/img/profiles/sam perez.png';
+import DrJacobVogelPng from '../../assets/img/profiles/dr. jacob vogel.png';
 
 export interface ProfilesProps {
     className?: string;
@@ -12,61 +15,83 @@ export interface ProfilesProps {
 export const Profiles = ({ className }: ProfilesProps) => {
     return (
         <div className={cx(styles.root, className)}>
-            <h1>Profiles</h1>
+            <h1 className={styles.header2}>Profiles</h1>
             <div className={styles.div3}>
                 <div>
                     <img
-                        src="https://static.wixstatic.com/media/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg/v1/fill/w_600,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg"
-                        alt=""
+                        src={JacoboHerreraJpg}
+                        alt="Jacobo Herrera.jpg"
                         className={styles['profile-pic']}
                     />
                 </div>
                 <div className={styles.div2}>
-                    <h2 className={styles.header1}>Heading 2</h2>
-                    <h3>Heading 3</h3>
+                    <h2 className={styles.header1}>Jacobo Herrera</h2>
+                    <h3>Executive Director</h3>
                     <p>
-                        This is a placeholder paragraph. It is here to provide structure while you
-                        work on your content. You can replace this text with your own words once you
-                        are ready. Placeholders like this are great for visualizing the layout and
-                        flow of your document before finalizing the content.
+                        Jacobo Herrera graduated from the University of Southern California (USC)
+                        with a bachelor’s degree in Music Industry. He has interned for the
+                        International Association of Venue Managers’ certification program as an
+                        operations assistant and at the Honda Center in Anaheim, CA as an Arena
+                        Management Intern, assisting the marketing, programming, and special events
+                        departments.  Jacobo will be continuing his education by pursuing a master’s
+                        in Arts Leadership from the Thornton School of Music and will be graduating
+                        May of 2025.
+                    </p>
+                </div>
+            </div>
+            <div className={styles.div3}>
+                <div>
+                    <img src={SamPerezPng} alt="Sam Perez.png" className={styles['profile-pic']} />
+                </div>
+                <div className={styles.div2}>
+                    <h2 className={styles.header1}>Sam Perez III</h2>
+                    <h3>Board Member</h3>
+                    <p>
+                        Samuel Pérez III is in his second year as a graduate student at USC and is
+                        serving as the drum major in his fourth year with the Spirit of Troy. After
+                        earning a Bachelor’s of Science in Mechanical Engineering from CSULB, he is
+                        currently pursuing a Masters in Music Teaching and Learning from the
+                        Thornton School of Music in hopes to further his skills as a music educator.
+                        Sam was previously the Assistant Director of Bands at Long Beach Polytechnic
+                        High School teaching the Poly Marching Unit and the Concert Band, as well as
+                        overseeing the program’s Winter Color Guard. His marching program was a 2x
+                        CSBC Finalist and his concert ensembles have earned Superior ratings at
+                        SCSBOA Band Festivals. Sam is is also currently a brass instructor for
+                        Pacific Crest Drum and Bugle Corps.
                     </p>
                 </div>
             </div>
             <div className={styles.div3}>
                 <div>
                     <img
-                        src="https://static.wixstatic.com/media/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg/v1/fill/w_600,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg"
-                        alt=""
+                        src={DrJacobVogelPng}
+                        alt="Dr. Jacob Vogel.png"
                         className={styles['profile-pic']}
                     />
                 </div>
                 <div className={styles.div2}>
-                    <h2 className={styles.header1}>Heading 2</h2>
-                    <h3>Heading 3</h3>
+                    <h2 className={styles.header1}>Dr. Jacob Vogel</h2>
+                    <h3>Board Member</h3>
                     <p>
-                        This is a placeholder paragraph. It is here to provide structure while you
-                        work on your content. You can replace this text with your own words once you
-                        are ready. Placeholders like this are great for visualizing the layout and
-                        flow of your document before finalizing the content.
-                    </p>
-                </div>
-            </div>
-            <div className={styles.div3}>
-                <div>
-                    <img
-                        src="https://static.wixstatic.com/media/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg/v1/fill/w_600,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_25e218a68cae48e98a21198d1280a6bd~mv2.jpg"
-                        alt=""
-                        className={styles['profile-pic']}
-                    />
-                </div>
-                <div className={styles.div2}>
-                    <h2 className={styles.header1}>Heading 2</h2>
-                    <h3>Heading 3</h3>
-                    <p>
-                        This is a placeholder paragraph. It is here to provide structure while you
-                        work on your content. You can replace this text with your own words once you
-                        are ready. Placeholders like this are great for visualizing the layout and
-                        flow of your document before finalizing the content.
+                        Dr. Jacob Vogel was named as Director of the USC Trojan Marching Band at the
+                        start of 2021, the first to be new to the position in over 50 years. This
+                        appointment followed a 12-year tenure as a teaching assistant, assistant
+                        director, associate director, and arranger with the program. Dr. Vogel also
+                        serves as an Associate Professor of Practice in Winds and Percussion with
+                        the Thornton School of Music, and as a member of the Music Teaching and
+                        Learning Faculty. In his former position as the Trojan Marching Band’s
+                        Associate Director and Arranger, Dr. Vogel divided his time between creating
+                        contemporary arrangements and contributing to the members’ continuing music
+                        education. Dr. Vogel’s arrangements have been electrifying the Coliseum for
+                        over a decade, and his student-centered approach to music teaching and
+                        learning has had a tremendous effect on the program. Dr. Vogel’s artistic
+                        vision has been instrumental to the success of his collaborative efforts
+                        with the Thornton School of Music’s Popular Music Program, the Kaufman
+                        School of Dance, X Ambassador’s Sam Harris, Fitz and the Tantrums, composers
+                        Ramin Djawadi and Alan Silvestri, as well as Angelina Jolie and Mark Hamill.
+                        Most recently, the Trojan Marching Band performed alongside Gwen Stefani and
+                        the Los Angeles Philharmonic, under the direction of Maestro Gustavo
+                        Dudamel.
                     </p>
                 </div>
             </div>
