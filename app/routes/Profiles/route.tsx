d608c27c34f1d27641f6a9a353543be161e1bfb8
Mu12Lambda11 <idjordan@cpp.edu>
2025-03-16 21:22:40 -0700

    Added a profiles page

---
+++ app/routes/Profiles/route.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Profiles as Profiles0 } from '../../../src/components/profiles/profiles';
+export default function Profiles() {
+    return (
+        <div>
+            <Profiles0 />
+        </div>
+    );
+}
